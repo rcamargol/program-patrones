ff03b67ce9e97c0bf885a5d2bf4304d6667bf578
ricar <ricar@SERVER-RDCL>
2024-07-29 18:42:01 -0500

    cambios para funcionamiento OK, prueba de escritorio

--- Repasito/src/Repaso.java
+++ Repasito/src/Repaso.java
@@ -1,37 +1,39 @@
 public class Repaso {
 
+
 	public static void main(String[] args) {
-		
 		int numeros[] = {1,2,3,4,5,6,7,8,9,10};
-		int [] numeros2 = new int [10];
-		
+		System.out.println("Original");
 		for (int i=0 ; i<10 ; i++) {
 			System.out.print(numeros[i]+" ");
 		}
+		System.out.println("\n");
+		System.out.println("i\taux\ta[i]\ta.length-i-1\ta[a.length-i-1]\t");
+		System.out.println("-----------------------------------------------------");
+		numeros = invertirArreglo(numeros);
 		
-		numeros2 = invertirArreglo(numeros);
-		
-		System.out.println("Invertido\n");
+		System.out.println("\nInvertido");
 		
 		for (int i=0 ; i<10 ; i++) {
-			System.out.print(numeros2[i]+" ");
+			System.out.print(numeros[i]+" ");
 		}
 		
 	}
 
 	public static int[] invertirArreglo(int[] a) {
 		int aux;
-		for (int i = 0 ; i<10 ; i++) {
-			aux = a[9-i];
-			a[9-i] = a[i];
+		int i;
+		for (i = 0 ; i<a.length/2 ; i++) {
+			aux = a[a.length-i-1];
+			a[a.length-i-1] = a[i];
 			a[i] = aux;
-			//System.out.println(aux);
-			//System.out.println("numero:"+a[i]);
-		}
-		for (int i=0 ; i<10 ; i++) {
-			System.out.print(a[i]+" ");
+			imprimirPruebaEsc(a,aux,i);
 		}
 		return a;
+	}
+	
+	public static void imprimirPruebaEsc(int[] a, int aux, int i) {
+		System.out.println(i+"\t"+aux+"\t"+a[i]+"\t\t"+(a.length-i-1)+"\t"+a[a.length-i-1]+"\t");
 	}
 	
 }
